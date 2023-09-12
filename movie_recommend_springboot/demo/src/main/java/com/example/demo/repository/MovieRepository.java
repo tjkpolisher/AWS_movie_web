81a0cfbce7e1b49b751043841216b45d1899e6b4
tjkpolisher <jktak5609@gmail.com>
2023-09-12 17:15:57 +0900

    Extension 추가

--- movie_recommend_springboot/demo/src/main/java/com/example/demo/repository/MovieRepository.java
+++ movie_recommend_springboot/demo/src/main/java/com/example/demo/repository/MovieRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.entity.Movie;
 
 @Repository
-public interface MovieRepository {
+public interface MovieRepository extends JpaRepository<Movie, String>{
     @Query(value="select * from movie_tbl m where m.title=:title limit 1", nativeQuery=true)
     public Movie findTitle(@Param(value = "title") String title);
 
