b703bd8b25141013efae44eca80e7645f18c5ce2
tjkpolisher <jktak5609@gmail.com>
2023-09-12 17:16:21 +0900

    불필요 임포트 제거

--- movie_recommend_springboot/demo/src/main/java/com/example/demo/entity/Movie.java
+++ movie_recommend_springboot/demo/src/main/java/com/example/demo/entity/Movie.java
@@ -1,6 +1,5 @@
 package com.example.demo.entity;
 
-import javax.management.loading.PrivateClassLoader;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
